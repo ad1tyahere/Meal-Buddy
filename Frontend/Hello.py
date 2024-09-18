8eb3ae5e25799254754a571f3c9fc32065e0477c
ad1tyahere <adityamishrahere340@gmail.com>
2024-09-18 11:42:45 +0530

    Initial commit

---
+++ Frontend/Hello.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Home",
+    page_icon="👋",
+)
+
+st.write("# Welcome to Food Recommendation using Chatbot! 👋")
+
+st.sidebar.warning("Select a recommendation app.")
+
+st.markdown(
+    """
+    """
+)
